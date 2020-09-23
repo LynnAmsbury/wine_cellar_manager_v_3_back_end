82645d43930032fd8e4c8c822e5a02ab4286692c
LynnAmsbury <Lynn8542@gmail.com>
2020-09-23 17:11:05 -0600

    Added  index and show routes for Wines. Created index and show logic with private method find_wine and before_action of find_wine for show method in the WinesController.

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,2 +1,20 @@
 class WinesController < ApplicationController
+
+  before_action :find_wine, only: [:show]
+
+  def index
+    @wines = Wine.all
+    render json: @wines
+  end
+
+  def show
+    render json: @wine
+  end
+
+  private
+
+  def find_wine
+    @wine = Wine.find(params[:id])
+  end
+
 end
